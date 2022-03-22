1b43df70a9e9461b7cbf4e79cacff924447224c8
heesungjang <heesungj7@gmail.com>
2022-03-23 00:54:07 +1000

    piece class added

---
+++ piece.js
@@ -0,0 +1,12 @@
+class Piece {
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
+    });
+  }
+}
